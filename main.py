d19471ff172de5f46af1842680ddce1987f8f883
dfrauu <dfrau001@gmail.com>
2022-11-11 13:23:50 -0500

    Renombrado y comentarios

--- main.py
+++ main.py
@@ -4,21 +4,28 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 import time
 
+# Establecer el navegador y sitio web
 driver = webdriver.Chrome('C:/Selenium/chromedriver')
 driver.maximize_window()
 driver.delete_all_cookies()
 driver.get("https://www.amazon.com/")
 
+# Seleccionar barra de busqueda e introducir texto
 driver.find_element(By.ID, "twotabsearchtextbox").send_keys("HP Pavilion azul")
 driver.find_element(By.ID, "nav-search-submit-button").click()
+# Tambien podemos agregar '+ Keys.ENTER' al final de la entrada de texto para hacer submit
 
+# Seleccionar el primer resultado mediante la imagen, Selenium selecciona la primera instancia por defecto
 driver.find_element(By.CLASS_NAME, "s-image").click()
 
-seleccion = Select(driver.find_element(By.ID, "quantity"))
-seleccion.select_by_index(1)
+# Cambiar la cantidad a 2, donde "1" es indice 0 y "2" es indice 1
+miSeleccion = Select(driver.find_element(By.ID, "quantity"))
+miSeleccion.select_by_index(1)
 
+# Agregar al carrito y verificar su contenido
 driver.find_element(By.ID, "add-to-cart-button").click()
 driver.find_element(By.CLASS_NAME, "a-button-text").click()
 
+# Finalizar la secuencia
 time.sleep(100)
 driver.close()
